d29cf70c915be18d28355b52ea69eecf78a4abd3
Wagner Elvio <wagnerelvio@gmail.com>
2022-04-15 17:36:45 -0300

    Primeiro Projeto EBAC

---
+++ src/primeiroprojeto/HelloWorld.java
@@ -0,0 +1,15 @@
+package primeiroprojeto;
+/*
+ * Primeiro Projeto EBAC - -
+ * Curso Desenvolvedor Back-End Java
+ * Aluno: Wagner Elvio
+ * 15/04/2022
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
